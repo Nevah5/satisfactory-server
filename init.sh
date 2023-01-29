af10b43f841ac65aa78dc60b7b8b0dfd5b79f11a
Nevah5 <neevaah5@gmail.com>
2023-01-29 15:32:45 +0100

    Removed check if PGID and PUID is 0

--- init.sh
+++ init.sh
@@ -47,17 +47,11 @@ mkdir -p \
 if ! [[ "$PGID" =~ $NUMCHECK ]] ; then
     printf "Invalid group id given: %s\\n" "$PGID"
     PGID="1000"
-elif [[ "$PGID" -eq 0 ]]; then
-    printf "PGID/group cannot be 0 (root)\\n"
-    exit 1
 fi
 
 if ! [[ "$PUID" =~ $NUMCHECK ]] ; then
     printf "Invalid user id given: %s\\n" "$PUID"
     PUID="1000"
-elif [[ "$PUID" -eq 0 ]]; then
-    printf "PUID/user cannot be 0 (root)\\n"
-    exit 1
 fi
 
 if [[ $(getent group $PGID | cut -d: -f1) ]]; then
